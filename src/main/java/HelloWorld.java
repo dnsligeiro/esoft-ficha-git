3d4e1e9b84957e559c5f59d8092daac09193e709
Dinis Cordeiro <2230677@my.ipleiria.pt>
2025-03-05 10:23:46 +0000

    cona

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld extends JFrame {
     private JLabel lblMensagem;
     private JPanel panel1;
 
+    private JLabel lblMensagem1;
     public HelloWorld(){
         setContentPane(panel1);
         pack();
